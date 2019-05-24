77961fb2ed27d01ef2a290217ffb4cd4b65d88d7
Víctor Córdova <theslapboy@gmail.com>
2019-05-24 16:05:37 -0700

    Get dates from a xml file

---
+++ GetDataHtml/FormHtml.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FormHtml.aspx.cs" Inherits="GetDataHtml.FormHtml" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home</title>
+    <style type="text/css">
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+       
+        <td><asp:Button ID="send" runat="server" Text="Submit" OnClick="send_Click" /></td>
+        
+    </form>
+</body>
+</html>
---
+++ GetDataHtml/FormHtml.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Net;
+using System.Net.Mail;
+
+namespace GetDataHtml
+{
+    public partial class FormHtml : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void send_Click(object sender, EventArgs e)
+        {
+            string to = string.Empty, subject = string.Empty, cc = string.Empty;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("~/MailList.xml"));
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/Emails/email");
+
+            foreach(XmlNode xmlNode in nodeList)
+            {
+                to = xmlNode.SelectSingleNode("to").InnerText;
+                cc = xmlNode.SelectSingleNode("cc").InnerText;
+                subject = xmlNode.SelectSingleNode("subject").InnerText;
+            }
+
+            SendEmail(to, "vicctor.cordova@gmail.com", subject, "TESSSST", cc);                    
+        }
+
+        protected void SendEmail(string to, string from,string subject, string body, string cc)
+        {            
+            string[] toCopies = to.Split(';');
+            MailMessage mailNew = new MailMessage();
+            mailNew.From = new MailAddress(from);
+
+            foreach (string toCopy in toCopies)
+            {
+                mailNew.To.Add(new MailAddress(toCopy));
+            }
+
+            mailNew.Bcc.Add(cc);
+            mailNew.Subject = subject;
+            mailNew.Body = body;
+            mailNew.IsBodyHtml = true;
+            mailNew.Priority = MailPriority.Normal;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential("theslapboy@gmail.com", "ateosolraiar");
+        }
+    }
+}
